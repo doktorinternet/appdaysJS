21d58ea11e8a1185d8d09a365d957d4d704540bf
Anton <wahlberg.anton@gmail.com>
2017-11-29 15:35:50 +0100

    Change MovieData variable names once again

--- data.ts
+++ data.ts
@@ -1,31 +1,31 @@
 class MovieData {
     constructor(
-        public Title: string,
-        public Image: string,
-        public Body: string,
-        public Cast: string[]) { }
+        public title: string,
+        public image: string,
+        public body: string,
+        public cast: string[]) { }
 }
 
-const title = "Waking life";
-const image = "https://resizing.flixster.com/riYUyvhcJIpEcryjp_6XX_Z-Tr0=/206x305/v1.bTsx" +
+const titel = "Waking life";
+const bild = "https://resizing.flixster.com/riYUyvhcJIpEcryjp_6XX_Z-Tr0=/206x305/v1.bTsx" +
     "MTIwODQ5MDtqOzE3NTg5OzEyMDA7MTE3OTsxNTcy";
-const body = "Cool little film about interesting corners of life, both awake and asleep." +
+const text = "Cool little film about interesting corners of life, both awake and asleep." +
     " Makes me think about nice things. Recommend to anyone who likes pretentions philosophy ;)";
-const cast = ["Ethan Hawke", "Trevor Jack Brooks", "Lorelei Linklater"];
+const actors = ["Ethan Hawke", "Trevor Jack Brooks", "Lorelei Linklater"];
 
 const wakingLife: MovieData = {
-    Body: body,
-    Cast: cast,
-    Image: image,
-    Title: title,
+    body: text,
+    cast: actors,
+    image: bild,
+    title: titel,
 };
 
 const predestination: MovieData = {
-    Body: "A little mindfucky, but not too much. Somewhat predictable, ut overall enjoyable as well",
-    Cast: ["Ethan Hawke (again..?)", "Sarah Snook", "Noah Taylor"],
-    Image: "http://is3.mzstatic.com/image/thumb/Video1/v4/55/90/be/5590beab-6bde-f520-" +
+    body: "A little mindfucky, but not too much. Somewhat predictable, ut overall enjoyable as well",
+    cast: ["Ethan Hawke (again..?)", "Sarah Snook", "Noah Taylor"],
+    image: "http://is3.mzstatic.com/mage/thumb/Video1/v4/55/90/be/5590beab-6bde-f520-" +
     "d93f -bbf97e2c1c89/source/1200x630bb.jpg",
-    Title: "Predestination",
+    title: "Predestination",
 };
 
 export const movieData = {
